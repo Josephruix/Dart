33d7ed383655f14295bb723752b504bca0cd2f84
Josephruix <josephruix24@gmai.com>
2024-04-18 11:49:42 -0500

    Dart

---
+++ Codigos.dart
@@ -0,0 +1,83 @@
+/*primer punto
+
+void main() {
+ 
+ const numero1=6;
+ const numero2=6;
+
+ var suma= numero1 + numero2;
+  print("el resultado de la suma es $suma");
+}
+*/
+/*
+Segundo punto
+void main() {
+  
+  double velocidad = 60.0; 
+  double tiempo = 2.5; 
+
+  
+  double distancia = calcularDistancia(velocidad, tiempo);
+
+ 
+  print("La distancia recorrida por el carro es de $distancia kilómetros.");
+}
+
+
+double calcularDistancia(double velocidad, double tiempo) {
+  
+  return velocidad * tiempo;
+ 
+}
+*/
+/*
+Tercer puntovoid main() {
+  int numero1= 3;
+  int numero2=2;
+  int numero3= 5;
+  
+  int suma= numero1 + numero2 + numero3;
+  
+  
+  
+  double promedio= suma/3;
+  
+  print("El promedio$promedio");
+  
+  
+  
+}*/
+/*Quinto Punto
+void main() {
+Map<String, dynamic> obj = {
+  "Producto1":{"Nombre":"Camisa","Precio":20000,"Color":"Rojo"},
+  "Producto2":{"Nombre":"Zapatos","Precio":60000,"Color":"Negro"},
+  "Producto3":{"Nombre":"Manilla","Precio":70000,"Color":"Azul"},
+  "Producto4":{"Nombre":"Pantalon","Precio":90000,"Color":"Verde"},
+  "Producto5":{"Nombre":"Gorra","Precio":50000,"Color":"Amarillo"},
+};
+  int suma;
+ suma= obj["Producto1"]["Precio"] + obj["Producto2"]["Precio"]+ obj["Producto3"]["Precio"]+obj["Producto4"]["Precio"]+obj["Producto5"]["Precio"];
+  print("el tota es de $suma");
+}*/
+/*Sexto Punto
+import "dart:math";
+void main() {
+  double catetoA = 3;
+  double catetoB = 4;
+  double calcularHipotenusa(double catetoA, double catetoB) {
+    double hipotenusa = sqrt((catetoA * catetoA) + (catetoB * catetoB));
+    return hipotenusa;
+  }
+
+  double hipotenusa = calcularHipotenusa(catetoA, catetoB);
+
+  print('La longitud de la hipotenusa es: $hipotenusa');
+}
+*/
+
+
+
+
+ 
+ 
